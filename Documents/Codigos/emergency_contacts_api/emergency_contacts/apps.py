384355b9f751f41e80f9f5d9df63f26105afc2fa
Thiago Ribeiro <dadosacademyacademy@gmail.com>
2024-11-05 11:12:29 -0300

    Delete Documents/Codigos directory

--- Documents/Codigos/emergency_contacts_api/emergency_contacts/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EmergencyContactsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'emergency_contacts'
--- Documents/Codigos/emergency_contacts_api/emergency_contacts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ContactCreate, ContactDelete
-
-urlpatterns = [
-    path('api/contacts/', ContactCreate.as_view(), name='contact-create'),
-    path('api/contacts/<int:pk>/', ContactDelete.as_view(), name='contact-delete'),
-]
